04120bbd2e16208168296952a0d5cfae4b09ff0a
Sam Smith <sesmith177@gmail.com>
2017-02-15 17:46:48 -0500

    WIP; Don't compile compile.go if buildpack is uncached

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,11 @@
 ROOTDIR="$( dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" )"
 BINDIR=$ROOTDIR/bin
 
+if [[ ! -e $ROOTDIR/dependencies ]]; then
+  echo "building uncached buildpack"
+  exit 0
+fi
+
 set -ex
 
 GOOS=linux go build -o $BINDIR/compile github.com/cloudfoundry/staticfile_buildpack
