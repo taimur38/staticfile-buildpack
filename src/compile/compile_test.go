75fd8a4afcc8f6cdaeb7c44842756d76e8168e47
Keaty Gross <kgross@pivotal.io>
2017-02-16 16:19:00 -0500

    WIP; add ginkgo skeleton
    Signed-off-by: Sam Smith <sesmith177@gmail.com>
    Signed-off-by: Keaty Gross <kgross@pivotal.io>

---
+++ src/compile/compile_test.go
@@ -0,0 +1,13 @@
+package main_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Compile", func() {
+	It("Is true", func() {
+		Expect(true).To(Equal(true))
+	})
+
+})
